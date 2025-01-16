42c3b721e148c09d78c8db995f14f93fe537d0da
jacopo <jacopo.cariato98@gmail.com>
2025-01-16 23:26:18 +0100

    first commit

---
+++ adkaora/src/main/java/com/generation/adkaora/AdkaoraApplication.java
@@ -0,0 +1,13 @@
+package com.generation.adkaora;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdkaoraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdkaoraApplication.class, args);
+    }
+
+}
---
+++ adkaora/src/test/java/com/generation/adkaora/AdkaoraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.adkaora;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdkaoraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
